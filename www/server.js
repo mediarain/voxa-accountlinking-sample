7c56e0887828212fa2438908c3b8e59002f537a9
Rommel Berrios <rommelb@rain.agency>
2017-07-12 14:28:50 -0600

    add lint

--- www/server.js
+++ www/server.js
@@ -4,10 +4,11 @@ const http = require('http');
 const app = require('./app');
 const env = require('../config/env.js');
 const config = require('../config');
+const debug = require('debug')('skill');
 
 const server = http.createServer(app);
-console.log(`Attempting to start.\r\n\tNode version: ${process.version}\r\n\tNODE_ENV: ${env}`);
+debug(`Attempting to start.\r\n\tNode version: ${process.version}\r\n\tNODE_ENV: ${env}`);
 
 server.listen(config.server.port, () => {
-  console.log('Server listening on port %d.', config.server.port);
+  debug('Server listening on port %d.', config.server.port);
 });
